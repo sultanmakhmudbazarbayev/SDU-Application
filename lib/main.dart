61c44e6e76562aa4cb451bc5d28b75b07abd123b
Alikhan Tokenov <alikhantokenov@gmail.com>
2021-10-21 01:13:32 +0600

    AlikhanTokenov-Pusj

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+import 'pages/bookmarks.dart';
+import 'pages/loading.dart';
+import 'pages/search.dart';
+import 'pages/home.dart';
+
+void main() => runApp(MaterialApp(
+      routes: {
+        '/': (context) => Loading(),
+      },
+    ));
