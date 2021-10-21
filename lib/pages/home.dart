d1f33eb54ae2105a2fb18071d085595eaa7acc49
Sula <190103178@stu.sdu.edu.kz>
2021-10-21 14:06:33 +0600

    HomePage changed

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,12 +7,97 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
+double heihtOfContainer = 120;
+
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      body: Text('This is home!'),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+            height: heihtOfContainer,
+            width: double.maxFinite,
+            child: Card(
+              child: Text(
+                "First lesson",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+              elevation: 5,
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+            height: heihtOfContainer,
+            width: double.maxFinite,
+            child: Card(
+              child: Text(
+                "Second lesson",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+              elevation: 5,
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+            height: heihtOfContainer,
+            width: double.maxFinite,
+            child: Card(
+              child: Column(
+                children: [
+                  Text(
+                    "Third lesson",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
+                ],
+              ),
+              elevation: 5,
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+            height: heihtOfContainer,
+            width: double.maxFinite,
+            child: Card(
+              child: Text(
+                "Fourth lesson",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+              elevation: 5,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
