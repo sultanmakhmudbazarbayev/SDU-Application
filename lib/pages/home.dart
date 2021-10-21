5b6ae58231ce989b6d7c1a8c4c12bc185e34bf09
Alikhan Tokenov <alikhantokenov@gmail.com>
2021-10-21 10:17:32 +0600

    Added-Nav-Bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'bookmarks.dart';
-import 'loading.dart';
-import 'search.dart';
-
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -12,52 +8,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<Widget> _screens = <Widget>[
-    Loading(),
-    Search(),
-    Bookmarks(),
-  ];
-  int _selectedItem = 0;
-
-  void _onItemTap(int item) {
-    setState(() {
-      _selectedItem = item;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('SDU Academy'),
-        centerTitle: true,
-        backgroundColor: Colors.grey,
-      ),
-      body: SafeArea(
-        child: _screens.elementAt(_selectedItem),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Search'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Bookmarks'),
-          ),
-        ],
-        currentIndex: _selectedItem,
-        onTap: _onItemTap,
-        selectedFontSize: 12,
-        unselectedFontSize: 12,
-        iconSize: 25,
-      ),
+      body: Text('This is home!'),
     );
-    ;
   }
 }
