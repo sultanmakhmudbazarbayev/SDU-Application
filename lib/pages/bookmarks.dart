0b30bc9e0757fdb0e337e20736cf006ea065d6fd
i10Beksultan <70829332+i10Beksultan@users.noreply.github.com>
2021-10-21 15:13:22 +0600

    Update bookmarks.dart

--- lib/pages/bookmarks.dart
+++ lib/pages/bookmarks.dart
@@ -16,17 +16,50 @@ class _BookmarksState extends State<Bookmarks> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      alignment: Alignment.center,
-      child: Text(
-        "Here will be your Bookmarks!",
-        style: TextStyle(
-          fontSize: 20.0,
-          fontStyle: FontStyle.normal,
-          color: Colors.grey,
-        ),
-      ),
-    ));
+    return LayoutBuilder(
+      builder: (context, constraint) {
+        return Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                  width: constraint.maxWidth,
+                  decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+
+                  child:Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text('Bookmarks',style: TextStyle(fontWeight: FontWeight.w600, fontSize: 22)),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  width: constraint.maxWidth,
+                  decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+
+                  child:Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text('Hello World!',style: TextStyle(fontWeight: FontWeight.w400, fontSize: 22)),
+                  ),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                SizedBox(height: 10),
+                Image.network("https://cdn.dribbble.com/users/876908/screenshots/7034541/media/a5dbf47103f35a82f8f0501ea78f25e7.png?compress=1&resize=1000x750",
+                  height: 256,
+                  fit: BoxFit.fitHeight,
+                ),
+                Text("Hello, Bex"),
+                Text("Hello")
+              ],
+            )
+          ],
+        );
+      },
+    );
   }
 }
