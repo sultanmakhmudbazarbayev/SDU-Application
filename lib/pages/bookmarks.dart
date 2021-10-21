b4a385715a844430d031a31dbd19605d4fcd8ac8
i10Beksultan <70829332+i10Beksultan@users.noreply.github.com>
2021-10-21 15:18:34 +0600

    Update bookmarks.dart
    Correct text

--- lib/pages/bookmarks.dart
+++ lib/pages/bookmarks.dart
@@ -41,20 +41,20 @@ class _BookmarksState extends State<Bookmarks> {
 
                   child:Padding(
                     padding: EdgeInsets.all(16.0),
-                    child: Text('Hello World!',style: TextStyle(fontWeight: FontWeight.w400, fontSize: 22)),
+                    child: Text('Download settings',style: TextStyle(fontWeight: FontWeight.w400, fontSize: 22)),
                   ),
                 )
               ],
             ),
             Column(
               children: [
-                SizedBox(height: 10),
+                SizedBox(height: 100),
                 Image.network("https://cdn.dribbble.com/users/876908/screenshots/7034541/media/a5dbf47103f35a82f8f0501ea78f25e7.png?compress=1&resize=1000x750",
                   height: 256,
                   fit: BoxFit.fitHeight,
                 ),
-                Text("Hello, Bex"),
-                Text("Hello")
+                Text("You have no bookmarks.",style: TextStyle(fontWeight: FontWeight.w600, fontSize: 26)),
+                Text("You can download videos in Bookmarks for offline viewing",style: TextStyle(fontWeight: FontWeight.w400, fontSize: 22))
               ],
             )
           ],
