43ad38da7638c36062275c196d809683548fae1f
Sula <190103178@stu.sdu.edu.kz>
2021-11-03 23:56:08 +0600

    alerts

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:project/pages/navigation_bar.dart';
 import 'package:project/services/auth_service.dart';
 import 'package:provider/provider.dart';
 
@@ -12,43 +16,61 @@ class LoginScreen extends StatelessWidget {
 
     final authService = Provider.of<AuthService>(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
+    _signin(emailController, passwordController) async {
+      String email = emailController.text;
+      String password = passwordController.text;
+      try {
+        await authService.signInWithEmailAndPassword(email, password);
+
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => NavigationBar()));
+      } on FirebaseAuthException catch (error) {
+        Fluttertoast.showToast(
+            msg: error.message, gravity: ToastGravity.CENTER);
+      }
+    }
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: "Email",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Login"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                labelText: "Password",
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                ),
               ),
             ),
-          ),
-          ElevatedButton(
-              onPressed: () {
-                authService.signInWithEmailAndPassword(
-                    emailController.text, passwordController.text);
-              },
-              child: Text('Login')),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/register');
-              },
-              child: Text('Register')),
-        ],
+            ElevatedButton(
+                child: Text('Login'),
+                onPressed: () => _signin(emailController, passwordController)),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/register');
+                },
+                child: Text('Register')),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:project/pages/navigation_bar.dart';
 import 'package:project/services/auth_service.dart';
 import 'package:provider/provider.dart';
 
@@ -12,40 +15,61 @@ class RegisterScreen extends StatelessWidget {
 
     final authService = Provider.of<AuthService>(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Register"),
+    _signup(emailController, passwordController) async {
+      String email = emailController.text;
+      String password = passwordController.text;
+      try {
+        await authService.createUserWithEmailAndPassword(email, password);
+
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => NavigationBar()));
+      } on FirebaseAuthException catch (error) {
+        Fluttertoast.showToast(
+            msg: error.message, gravity: ToastGravity.CENTER);
+      }
+    }
+
+      return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: "Email",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Register"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                labelText: "Password",
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                ),
               ),
             ),
-          ),
-          ElevatedButton(
-              onPressed: () async {
-                await authService.createUserWithEmailAndPassword(
-                    emailController.text, passwordController.text);
-                Navigator.pop(context);
-              },
-              child: Text('Register')),
-        ],
+            ElevatedButton(
+              child: Text('Register'),
+                onPressed: () => _signup(emailController, passwordController)
+                ),
+          ],
+        ),
       ),
     );
+
+
+    }
   }
-}
+
