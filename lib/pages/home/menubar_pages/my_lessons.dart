ceb6e2ad2caa1da0a44856357710b955ca73ae88
Sula <190103178@stu.sdu.edu.kz>
2021-11-17 23:46:15 +0600

    menubar_pages

---
+++ lib/pages/home/menubar_pages/my_lessons.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyLessons extends StatefulWidget {
+  const MyLessons({Key key}) : super(key: key);
+
+  @override
+  _MyLessonsState createState() => _MyLessonsState();
+}
+
+class _MyLessonsState extends State<MyLessons> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/home/menubar_pages/qasduacademy.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QASDUAcademy extends StatefulWidget {
+  const QASDUAcademy({Key key}) : super(key: key);
+
+  @override
+  _QASDUAcademyState createState() => _QASDUAcademyState();
+}
+
+class _QASDUAcademyState extends State<QASDUAcademy> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
